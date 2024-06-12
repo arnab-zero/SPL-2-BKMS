f07e63ed976748126d4f126cdda89fa8c5b6996c
arnab-zero <darnabhere@gmail.com>
2024-06-12 13:37:41 +0600

    Created new Neo4j instance and reconnected it.

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
-const neo4j = require('neo4j-driver');
-require('dotenv').config({
-  path: 'Neo4j-4d2abc3a-Created-2024-05-02.txt',
-  debug: true // to raise file/parsing errors
+const neo4j = require("neo4j-driver");
+require("dotenv").config({
+  path: "Neo4j-70a45c9e-Created-2024-06-12.txt",
+  debug: true, // to raise file/parsing errors
 });
 
 const URI = process.env.NEO4J_URI;
@@ -13,7 +13,7 @@ async function connectToDatabase() {
   try {
     driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
     const serverInfo = await driver.getServerInfo();
-    console.log('Connection established');
+    console.log("Connection established");
     console.log(serverInfo);
     return driver;
   } catch (err) {
@@ -23,5 +23,5 @@ async function connectToDatabase() {
 }
 
 module.exports = {
-  connectToDatabase
+  connectToDatabase,
 };
