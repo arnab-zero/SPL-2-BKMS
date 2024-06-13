fc91a839f2f6efb72b501ac83791003652e01857
arnab-zero <darnabhere@gmail.com>
2024-06-13 20:57:12 +0600

    updated legend in graph

--- frontend/src/pages/GraphLegend.jsx
+++ frontend/src/pages/GraphLegend.jsx
@@ -2,12 +2,12 @@ const GraphLegend = () => {
   return (
     <div className="bg-white shadow-md rounded-lg px-24 py-6 flex flex-col items-center mt-5 text-xl font-semibold w-fit">
       <h2>
-        <span className="font-bold text-[#c1793f]">Node</span> indicate paper
-        node
+        <span className="font-bold text-[#c1793f]">Node</span> indicates paper
+        topic node
       </h2>
       <h2>
-        <span className="font-bold text-[#DaB495]">Node</span> indicate paper
-        topic node
+        <span className="font-bold text-[#DaB495]">Node</span> indicates paper
+        node
       </h2>
     </div>
   );
